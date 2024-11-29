5263f0c57dfd509ef5866c9011a82e877d1c11f7
Dan Feinstein <otherdaniel@gmail.com>
2024-11-28 23:09:34 -0800

    test1234

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import './components/HUD.scss'
 function App() {
   return (
     <div style={{ width: '100vw', height: '100vh' }}>
+      <h1 style={{position: 'absolute', top: 0, left: 0, color: 'white'}}>Hello</h1>
       <HUD />
       <UpgradeSelection />
       <Canvas shadows>
